dff9411411208ff98ea61aa571eadf7f10ae986c
justdovlet <dovlet320@gmail.com>
2022-03-02 16:54:32 +0300

    Added parameter "name" for GET request

--- src/main/java/com/example/rest/GreetingController.java
+++ src/main/java/com/example/rest/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,7 +18,7 @@ public class GreetingController {
 
 
     @GetMapping("/greeting")
-    Greeting greeting(){
-        return new Greeting(counter.incrementAndGet(), String.format(template, "World"));
+    Greeting greeting(@RequestParam(value= "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
